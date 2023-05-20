839aea3b1ebc447eeacd4e6830a44e7fe148a0ae
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-05-20 20:35:55 -0300

    test: github action link valids

--- test/uniswap-default.test.js
+++ test/uniswap-default.test.js
@@ -5,9 +5,31 @@ const { getAddress } = require("@ethersproject/address");
 const Ajv = require("ajv");
 const buildList = require("../src/buildList");
 const jp = require("jsonpath");
+const axios = require("axios");
 
 const ajv = new Ajv({ allErrors: true, format: "full" });
 const validate = ajv.compile(schema);
+
+function getBranchFromArgs(defaultBranch) {
+  // refs/tags/<tag_name> // refs/heads/<branch_name>
+  const args = process.argv.slice(2); // Ignorando os dois primeiros argumentos (node e nome do arquivo)
+  const branchIndex = args.indexOf("--branch");
+  console.log("Args", args);
+  if (branchIndex !== -1 && branchIndex < args.length - 1) {
+    const branch = args[branchIndex + 1];
+    console.log("Branch/Tag found", branch);
+    if (branch) {
+      if (branch.startsWith("refs/heads/")) {
+        return branch.replace("refs/heads/", "");
+      } else if (branch.startsWith("refs/tags/")) {
+        return branch.replace("refs/tags/", "");
+      }
+    }
+    return branch;
+  }
+
+  return defaultBranch;
+}
 
 describe("buildList", () => {
   const defaultTokenList = buildList();
@@ -73,5 +95,39 @@ describe("buildList", () => {
     expect(packageJson.version).to.equal(
       `${defaultTokenList.version.major}.${defaultTokenList.version.minor}.${defaultTokenList.version.patch}`
     );
+  });
+
+  it("all images url are valid", async () => {
+    for (let token of defaultTokenList.tokens) {
+      expect(token.logoURI).to.match(/^https?:\/\/.+/);
+    }
+  });
+
+  it("all images return status 200", async function () {
+    this.timeout(0);
+    const branch = getBranchFromArgs(undefined);
+    for (let token of defaultTokenList.tokens) {
+      let url = token.logoURI;
+      // if tokenURI have that format: *1Hive/default-token-list/master/src/assets/* then replace to *1Hive/default-token-list/{branch}/src/assets*
+      if (branch) {
+        if (url.includes("1Hive/default-token-list/master/src/assets/")) {
+          url = url.replace(
+            "1Hive/default-token-list/master/src/assets/",
+            `1Hive/default-token-list/${branch}/src/assets/`
+          );
+        }
+      }
+
+      try {
+        const response = await axios.get(url, { timeout: 20000 });
+        if (response.status === 200) {
+          // console.log(`URL ${url} é válida.`);
+        } else {
+          console.log(`URL ${url} retornou um status diferente de 200.`);
+        }
+      } catch (error) {
+        console.error(`Erro ao verificar a URL ${url}: ${error.message}`);
+      }
+    }
   });
 });
